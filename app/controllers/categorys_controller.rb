fc017e195aaacd26059a0134fb247c01cf702ba8
ei1125 <i.dont.like.monday1125@gmail.com>
2020-03-02 22:02:27 +0900

    set_action作成

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -1,18 +1,22 @@
 class CategorysController < ApplicationController
 
+  before_action :set_category, only: [:show]
+
   def index
     @categorys = Category.order("id ASC")
     @category1s = @categorys.limit(13)
     
   end
   
-
-
   def show
-    @category = Category.find(params[:id])
     @items = selected_category_items(@category)
     @items_brand = category_brands(@items).sort { |a, b| a[:name] <=> b[:name] }
   end
 
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
 end
