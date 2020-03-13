4dd0c2c6f116d5e5f1b0e4afc14037043c9ef1fe
yuna-kusuhara <narlocked.222.b@gmail.com>
2020-03-14 00:30:33 +0900

    購入画面と導線の作成

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,16 +1,21 @@
 class PurchaseController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_card
   before_action :set_item
 
   require 'payjp'
 
   def index
+    if @item.trading.status == false
+      redirect_to root_path
+    end
     if @card.blank?
       redirect_to new_card_path 
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @default_card = customer.cards.retrieve(@card.card_id)
+      @image = @item.images.first
     end
   end
 
@@ -21,6 +26,9 @@ class PurchaseController < ApplicationController
     customer: @card.customer_id,
     currency: 'jpy',
     )
+    @trading = @item.trading
+    @trading.status = false
+    @trading.save
   end
 
   def set_card
