142e3d09207dcd6e7004ebfb3bbdb4aa99e2001e
ei1125 <i.dont.like.monday1125@gmail.com>
2020-03-14 21:56:29 +0900

    修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,5 @@
 class Favorite < ApplicationRecord
-  validates :item_id, uniqueness: { scope: [:user_id, :item_id]}
+  validates :item_id, uniqueness: { scope: :user_id }
 
   belongs_to :user
   belongs_to :item
