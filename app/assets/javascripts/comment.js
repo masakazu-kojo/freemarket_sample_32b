305110e0844dcd00693f6ae827b185160a8b7406
ei1125 <i.dont.like.monday1125@gmail.com>
2020-03-16 23:23:22 +0900

    timestanp作成

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,6 +1,6 @@
 $(function(){
   // コメント貼り付け用
-  function appendComment(userName,comment){
+  function appendComment(userName,comment,createdAt,timeAgo){
     let html = `<li class="message-list"><div class="message-user">
                     <div class="message-user__name">
                       ${userName}
@@ -10,11 +10,19 @@ $(function(){
                     <div class="message-body-text">
                       ${comment}
                     </div>
+                    <div class="message-icons">
+                      <div class="message-icon-left">
+                        <i class="far fa-clock"></i>
+                        <time datetime="${createdAt}">
+                          ${timeAgo}前
+                        </time>
+                      </div>
+                    </div>
                   </div></li>`;
     return html;
   }
   // 出品者のコメント貼り付け用
-  function appendSellerComment(userName,comment){
+  function appendSellerComment(userName,comment,createdAt,timeAgo){
     let html = `<li class="message-list"><div class="message-user">
                     <div class="message-user__name">
                       ${userName}
@@ -27,10 +35,17 @@ $(function(){
                     <div class="message-body-text">
                       ${comment}
                     </div>
+                    <div class="message-icons">
+                      <div class="message-icon-left">
+                        <i class="far fa-clock"></i>
+                        <time datetime="${createdAt}">
+                          ${timeAgo}前
+                        </time>
+                      </div>
+                    </div>
                   </div></li>`;
     return html;
   }
-
   $('#comment-button').on('click', function(){
     if ($('#comment-form').val() != ""){
       $('.comment-error').css('display','none');
@@ -47,14 +62,16 @@ $(function(){
       .done(function(t_data){
         let userName = t_data.userName;
         let comment = t_data.comment;
+        let createdAt = t_data.created_at;
+        let timeAgo = t_data.timeAgo;
         // textarea削除
         $('#comment-form').val("");
         if (currentId == itemUserId) {
         // 出品者のコメント貼り付け
-        $('.message-items').append(appendSellerComment(userName,comment));
+        $('.message-items').append(appendSellerComment(userName,comment,createdAt,timeAgo));
         }else{
         // コメント貼り付け
-        $('.message-items').append(appendComment(userName,comment));
+        $('.message-items').append(appendComment(userName,comment,createdAt,timeAgo));
         }
       })
     }else{
